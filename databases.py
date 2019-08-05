31ed97494d1c867a5da311e2573607c1ec309f43
myname21-meet <myname21@meet.mit.edu>
2019-08-05 06:29:03 -0400

    test

---
+++ databases.py
@@ -0,0 +1,18 @@
+from model import Base, Student
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///students.db?check_same_thread=False')
+Base.metadata.create_all(engine)
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+def add_product(name, price, maker, picture):
+  product_object = Product(
+    name=name,
+    price=price,
+    maker=maker,
+    picture=picture)
+  session.add(user_object)
+  session.commit()
